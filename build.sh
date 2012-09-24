03dc253b3af6084cba2b9261f1463ce9d5624f0a
Mark Kennard <komcomputers@gmail.com>
2012-09-24 15:08:22 +1200

    build: Add build script

--- build.sh
+++ build.sh
@@ -1,40 +1,88 @@
-#!/bin/bash
+#!/bin/sh
+#
+# Kernel build script
+#
+# By Mark "Hill Beast" Kennard
+#
 
-HWVER=rev_a
-CLEAN=""
+TOOLCHAIN=/usr/arm-4.4.3-toolchain/bin/arm-eabi-
+ARCH=arm
+MKBOOTIMG=./mkbootimg
+RAMDISK=usr/ramdisk.cpio.lzma
+BOOTIMAGE=normalboot.img
 
+CPUCORES=`grep "cpu cores" /proc/cpuinfo | awk '{ print $4 }' | head -c 1`
 
-WORK_PATH=`pwd`
-#IMAGE_PART1='../../../../../android/images/p970'
-IMAGE_PART1='./'
+USEOUTDIR=`echo $1 $2 $3 $4 | grep -useout`
 
+if [ -z $USEOUTDIR ]; then
+	echo "Make will output to kernel directory"
+else
+	OUTDIR=/mnt/out/out
+	OUTSUBDIR=`cat .git/config | grep url | sed "s|/| |" | awk '{ print $4 }'`
+	USEOUTDIR="$OUTDIR/kernel/$OUTSUBDIR/"
+	echo "Using output directory ($USEOUTDIR)"
+	mkdir -p $USEOUTDIR 2> /dev/null
+fi
 
-for argument in "$@"
-do
-	case "$argument" in
-    hw*)
-      HWVER=${argument:2} 
-      ;;
-	"clean")
-	  CLEAN=clean 
-	  make clean
-	  exit 0
-	  ;;
-	esac
-done
+if [ -z $1 ]; then
+	if [ -z $KBUILD_BUILD_VERSION ]; then
+		export KBUILD_BUILD_VERSION="Test-`date '+%Y%m%d-%H%M'`"
+	fi
+	echo "Kernel will be labelled ($KBUILD_BUILD_VERSION)"
+else
+	if [ $1 = "config" ]; then
+		if [ -z "$USEOUTDIR" ]; then
+			make menuconfig CROSS_COMPILE=$TOOLCHAIN ARCH=$ARCH
+		else
+			make menuconfig CROSS_COMPILE=$TOOLCHAIN ARCH=$ARCH O=$USEOUTDIR
+		fi
+		exit
+	fi
+	if [ $1 = "saveconfig" ]; then
+		if [ -z $2 ]; then
+			echo "You need to specify a defconfig filename"
+			echo "./build.sh saveconfig [x_defconfig]"
+			exit
+		fi
+		if test -f arch/$ARCH/configs/$2; then
+			cp arch/$ARCH/configs/$2 arch/$ARCH/configs/$2.bak
+		fi
+		grep "=" .config > arch/$ARCH/configs/$2
+		echo ".config saved to arch/$ARCH/configs/$2"
+		exit
+	fi
+	echo "Setting kernel name to ($1)"
+	export KBUILD_BUILD_VERSION=$1
+fi
 
+echo "Compiling the kernel"
+if test -f $USEOUTDIR"arch/$ARCH/boot/zImage"; then
+	rm $USEOUTDIR"arch/$ARCH/boot/zImage"
+fi
 
-echo "##############building kernel HWVER = ${HWVER} ####################"
+if [ -z "$USEOUTDIR" ]; then
+	echo "make -j$CPUCORES CROSS_COMPILE=$TOOLCHAIN ARCH=$ARCH"
+	make -j$CPUCORES CROSS_COMPILE=$TOOLCHAIN ARCH=$ARCH
+else
+	echo "make -j$CPUCORES CROSS_COMPILE=$TOOLCHAIN ARCH=$ARCH O=$USEOUTDIR"
+	make -j$CPUCORES CROSS_COMPILE=$TOOLCHAIN ARCH=$ARCH O=$USEOUTDIR
+fi
 
-#make ${CLEAN} ARCH=arm CROSS_COMPILE=arm-none-linux-gnueabi- sniper_${HWVER}_emmc_defconfig
-#make ${CLEAN} ARCH=arm CROSS_COMPILE=arm-none-linux-gnueabi- android_zoom3_defconfig
-make ${CLEAN} ARCH=arm CROSS_COMPILE=arm-none-linux-gnueabi- black_p970_defconfig
-make ${CLEAN} -j8 ARCH=arm CROSS_COMPILE=arm-none-linux-gnueabi- uImage
-make ${CLEAN} ARCH=arm CROSS_COMPILE=arm-none-linux-gnueabi- modules
+echo $USEOUTDIR
+if test -f $USEOUTDIR"arch/arm/boot/zImage"; then
+	TARBALL=$KBUILD_BUILD_VERSION-boot.tar
 
-#if [ ! -d ${IMAGE_PART1} ]
-#then
-#	mkdir ${IMAGE_PART1}
-#fi
+	cd usr
+	./gen_ramdisk.sh > /dev/null 2> /dev/null
+	cd ..
 
-#cp -vf arch/arm/boot/uImage ${IMAGE_PART1}
+	echo "  BOOTIMG $BOOTIMAGE using ramdisk ($RAMDISK)"
+	$MKBOOTIMG --kernel $USEOUTDIR"arch/arm/boot/zImage" --ramdisk $RAMDISK --pagesize 1000 -o $BOOTIMAGE
+	echo "  TAR     $TARBALL"
+	tar cf $TARBALL $BOOTIMAGE
+else
+	echo "Will not tarball as make didn't produce zImage"
+fi
+
+echo "Done"
